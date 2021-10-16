8d979447d9520f5948e50994f315e02c9c1b10b5
zuocheng li <zuocheng.li@sjsu.edu>
2021-10-16 11:01:43 +0800

    add test for mean

--- Test/testMean.java
+++ Test/testMean.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class testMean {
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testMean {
+    @Test
+    public void testMean(){
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(3);
+        arr.add(3);
+        arr.add(3);
+        assertEquals(3, ArrayListUtils.mean(arr), "mean should be 3");
+    }
 }
